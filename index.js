134bcff62f718c378ff0bf7f407c0d633f92759e
pandiji <skm808478@gmail.com>
2023-07-01 03:42:44 +0530

    final

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
 const path=require("path")
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname+"/public")))
 const db = mysql.createConnection({
-  host: 'containers-us-west-135.railway.app',
-  port: '6251',
-  user: 'root',
-  password: 'rIKjp6Jje5yismcrUApm',
-  database: 'railway',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
   waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
